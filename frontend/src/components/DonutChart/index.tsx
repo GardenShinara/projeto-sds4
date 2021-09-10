77ca661f27b1588933efe72514d1a46c5e35b718
GardenShinara <shi.nara@hotmail.com>
2021-09-10 16:55:10 -0400

    Env var.

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,28 +11,24 @@ type ChartData = {
 
 function DonutChart() {
 
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     useEffect(() => {
-        axios.get(BASE_URL + "/Sales/amount-by-seller")
+        axios.get(`${BASE_URL}/Sales/amount-by-seller`)
             .then(response => {
                 const data = response.data as SaleSum[];
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
+
                 setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
-                console.log(chartData);
-            })
-
-
-    },[]);
-    
     const options = {
         legend: {
             show: true
         }
     }
-
 
     return (
         <Chart
