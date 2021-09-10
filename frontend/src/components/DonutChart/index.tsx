3502cdac30c3f8344b7dfeb274476a0cdcc07261
GardenShinara <shi.nara@hotmail.com>
2021-09-10 15:03:53 -0400

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,34 +1,33 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
-type ChartData ={
+type ChartData = {
     labels: string[];
     series: number[];
 }
 
 function DonutChart() {
 
-    let chartData: ChartData ={ labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
-    axios.get(BASE_URL + "/Sales/amount-by-seller")
-        .then(response=>{
-            const data = response.data as SaleSum[];
-            const myLabels =data.map(x => x.sellerName);
-            const mySeries =data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(BASE_URL + "/Sales/amount-by-seller")
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData ={labels: myLabels, series: mySeries};
+                setChartData({ labels: myLabels, series: mySeries });
 
-            console.log(response.data);
-        })
+                console.log(chartData);
+            })
 
 
-  //  const mockData = {
-   //     series: [477138, 499928, 444867, 220426, 473088],
-   //     labels: ['Miller', 'Dlucca', 'Portman', 'Mayers', 'Jardim']
-   // }
-    
+    }, [])
+
     const options = {
         legend: {
             show: true
@@ -37,8 +36,8 @@ function DonutChart() {
 
 
     return (
-        <Chart 
-            options={{...options, labels:chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
